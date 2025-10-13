92ea6fc05303dfd5b06966b973a7be7ae57abee7
Efrain012 <efrain.dominguez012@gmail.com>
2025-10-13 12:14:06 -0700

    Exportación del modelo

--- topic-scoring/src/app.py
+++ topic-scoring/src/app.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
 import time
-
-class backend:
-    @staticmethod
-    def GenerateNumbers():
-        # CONVERTIR A LISTA PARA MANEJAR EN JSON
-        return np.random.randint(0,100,5).tolist()
+from modules.backend import Backend
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for local testing
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,9 +18,10 @@ class InputData(BaseModel):
     text: str
 
 @app.post("/ai")
-def home(data: InputData):
+def analizar_sentimiento(data: InputData):
+    resultado = Backend.predict_sentiment(data.text) 
     return {
         "input": data.text,
-        "scores": backend.GenerateNumbers(),  # ✅ CHANGED HERE
+        "resultado": resultado,
         "timestamp": time.time()
     }
--- topic-scoring/src/modules/backend.py
+++ topic-scoring/src/modules/backend.py
@@ -1,7 +1,21 @@
+import tensorflow as tf
 import numpy as np
+
+model = tf.keras.models.load_model("src/modules/sentiment_model.keras")
+print("Modelo cargado")
 
 
 class Backend:
-    
-    def generate_array():
-        return np.random.randint(0, 100, 5) 
+    @staticmethod
+    def predict_sentiment(text: str):
+        """Analiza el sentimiento del texto"""
+        texto_tensor = tf.constant([text])
+        pred = model.predict(texto_tensor, verbose=0)
+        score = tf.sigmoid(pred[0][0]).numpy()
+        sentimiento = "positivo" if score > 0.5 else "negativo"
+        confianza = round(float(score if score > 0.5 else 1 - score), 3)
+
+        return {
+            "sentimiento": sentimiento,
+            "confianza": confianza
+        }
